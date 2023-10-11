8dd92cbb9b2b6ae0c52c386cca504b16209bade7
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-10-11 15:40:43 +0300

    knn-classifier

--- knn-classifier.go
+++ knn-classifier.go
@@ -7,34 +7,53 @@ import (
 	"github.com/sjwhitworth/golearn/knn"
 )
 
-// simple iris knn classifier
-func main() {
-	data, err := base.ParseCSVToInstances("Iris.csv", true)
+func KnnClassifier(dataset string) (base.FixedDataGrid, map[string]map[string]int) {
+	data, err := base.ParseCSVToInstances(dataset, true)
+
 	if err != nil {
 		panic(err)
-		return
+		return nil, nil
 	}
-	//a summary of the data
-	fmt.Println(data)
+
+	//summary of the
+	fmt.Printf("Data:\n%s", data)
+
 	knnModel := knn.NewKnnClassifier("euclidean", "linear", 2)
 
-	trainD, testD := base.InstancesTrainTestSplit(data, 0.70)
-	err = knnModel.Fit(trainD)
-	if err != nil {
-		panic(err)
-		return
-	}
-	predictions, err := knnModel.Predict(testD)
-	if err != nil {
-		panic(err)
-		return
-	}
+	trainData, testData := base.InstancesTrainTestSplit(data, 0.50)
 
-	fmt.Printf("Predictions:\n%s", predictions)
-	confusionMat, err := evaluation.GetConfusionMatrix(predictions, testD)
+	err = knnModel.Fit(trainData)
 	if err != nil {
 		panic(err)
-		return
+		return nil, nil
 	}
-	fmt.Printf("Summary\n%s", evaluation.GetSummary(confusionMat))
+	predictions, err := knnModel.Predict(testData)
+	if err != nil {
+		panic(err)
+		return nil, nil
+	}
+	confusionMatrix, _ := evaluation.GetConfusionMatrix(predictions, testData)
+
+	return predictions, confusionMatrix
 }
+
+// simple iris knn classifier
+func main() {
+	predictions, confusionMat := KnnClassifier("Iris.csv")
+	fmt.Printf("Predictions:\n%s", predictions)
+	fmt.Printf("\nConfusion Matrix:\n%s", evaluation.GetSummary(confusionMat))
+}
+
+//sample output
+//Confusion Matrix:
+//Reference Class True Positives  False Positives True Negatives  Precision      Recall                                                                        F
+//1 Score
+//--------------- --------------  --------------- --------------  ---------      ------                                                                        -
+//-------
+//Iris-versicolor 21              1               52              0.9545         0.9130                                                                        0
+//.9333
+//Iris-virginica  26              2               48              0.9286         1.0000                                                                        0
+//.9630
+//Iris-setosa     26              0               49              1.0000         0.9630                                                                        0
+//.9811
+//Overall accuracy: 0.9605
