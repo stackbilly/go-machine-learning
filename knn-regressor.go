4912b9dcaa2e82971a70e73952f2e8b5c30e00fb
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-10-11 17:30:14 +0300

    knn-regressor

--- knn-regressor.go
+++ knn-regressor.go
@@ -2,27 +2,52 @@ package main
 
 import (
 	"fmt"
-	"github.com/sjwhitworth/golearn/base"
-	"github.com/sjwhitworth/golearn/knn"
-	"math/rand"
-	"time"
+	mongoimport "github.com/Livingstone-Billy/mongo-import"
+	"strings"
 )
 
+type Insurance struct {
+	Age      int
+	Sex      string
+	Bmi      float64
+	Children int
+	Smoker   int
+	Region   string
+	Charges  float64
+}
+
+func loadDataset(filename string) ([]Insurance, error) {
+	records, err := mongoimport.CSVReader("datasets/insurance.csv")
+	if err != nil {
+		return nil, err
+	}
+	//parse csv data into a slice of house objects
+	insurances := []Insurance{}
+	for _, line :=range strings.Split()
+}
+
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	//rand.Seed(time.Now().UnixNano())
+	//
+	////load dataset
+	//data, err := base.ParseCSVToInstances("/datasets/insurance.csv", true)
+	//
+	//if err != nil {
+	//	panic(err)
+	//	return
+	//}
+	//
+	//fmt.Printf("data:\n%s", data)
+	//
+	//trainData, testData := base.InstancesTrainTestSplit(data, 0.80)
+	//knnRegr := knn.NewKnnRegressor("euclidean")
+	//
+	//err = knnRegr.Fit(trainData)
 
-	//load dataset
-	data, err := base.ParseCSVToInstances("/datasets/insurance.csv", true)
-
+	records, err := mongoimport.CSVReader("datasets/insurance.csv")
 	if err != nil {
 		panic(err)
 		return
 	}
-
-	fmt.Printf("data:\n%s", data)
-
-	trainData, testData := base.InstancesTrainTestSplit(data, 0.80)
-	knnRegr := knn.NewKnnRegressor("euclidean")
-
-	err = knnRegr.Fit(trainData)
+	fmt.Printf("Records:\n%s", records)
 }
