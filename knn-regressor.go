22e1657178a4d9fa3e77f01696db37bb6230f5f0
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-10-12 11:42:18 +0300

    mat.Dense

--- knn-regressor.go
+++ knn-regressor.go
@@ -2,52 +2,85 @@ package main
 
 import (
 	"fmt"
-	mongoimport "github.com/Livingstone-Billy/mongo-import"
+	"github.com/sjwhitworth/golearn/knn"
+	"io/ioutil"
+	"strconv"
 	"strings"
 )
 
 type Insurance struct {
-	Age      int
-	Sex      string
+	Age      float64
+	Sex      float64
 	Bmi      float64
-	Children int
-	Smoker   int
-	Region   string
+	Children float64
+	Smoker   float64
+	Region   float64
 	Charges  float64
 }
 
 func loadDataset(filename string) ([]Insurance, error) {
-	records, err := mongoimport.CSVReader("datasets/insurance.csv")
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
+		panic(err)
 		return nil, err
 	}
-	//parse csv data into a slice of house objects
-	insurances := []Insurance{}
-	for _, line :=range strings.Split()
+	var insurances []Insurance
+	for _, line := range strings.Split(string(data), "\n") {
+		parts := strings.Split(line, ",")
+		insurance := Insurance{}
+		insurance.Age, _ = strconv.ParseFloat(parts[0], 64)
+		insurance.Sex, _ = strconv.ParseFloat(parts[1], 64)
+		insurance.Bmi, _ = strconv.ParseFloat(parts[2], 64)
+		insurance.Children, _ = strconv.ParseFloat(parts[3], 64)
+		insurance.Smoker, _ = strconv.ParseFloat(parts[4], 64)
+		insurance.Region, _ = strconv.ParseFloat(parts[5], 64)
+		insurance.Charges, _ = strconv.ParseFloat(parts[6], 64)
+		insurances = append(insurances, insurance)
+	}
+	return insurances, err
 }
 
 func main() {
-	//rand.Seed(time.Now().UnixNano())
-	//
-	////load dataset
-	//data, err := base.ParseCSVToInstances("/datasets/insurance.csv", true)
-	//
-	//if err != nil {
-	//	panic(err)
-	//	return
-	//}
-	//
-	//fmt.Printf("data:\n%s", data)
-	//
-	//trainData, testData := base.InstancesTrainTestSplit(data, 0.80)
-	//knnRegr := knn.NewKnnRegressor("euclidean")
-	//
-	//err = knnRegr.Fit(trainData)
-
-	records, err := mongoimport.CSVReader("datasets/insurance.csv")
+	//data, err := base.ParseCSVToInstances("datasets/insurance.csv", true)
+	data, err := loadDataset("datasets/insurance.csv")
 	if err != nil {
 		panic(err)
 		return
 	}
-	fmt.Printf("Records:\n%s", records)
+	fmt.Printf("Data:\n%v", data)
+
+	var age []float64
+	for _, val := range data {
+		age = append(age, val.Age)
+	}
+
+	var bmi []float64
+	for _, val := range data {
+		bmi = append(bmi, val.Bmi)
+	}
+
+	var sex []float64
+	for _, val := range data {
+		sex = append(sex, val.Sex)
+	}
+
+	var targets []float64
+	for _, val := range data {
+		targets = append(targets, val.Charges)
+	}
+
+	var trainData []float64
+	trainData = append(trainData, age...)
+	trainData = append(trainData, sex...)
+	trainData = append(trainData, bmi...)
+
+	knnReg := knn.NewKnnRegressor("euclidean")
+
+	knnReg.Fit(trainData, targets, len(trainData), 3)
+
+	gender, _ := strconv.ParseFloat("female", 64)
+
+	var newInstance = []float64{34, gender, 34.5}
+
+	predictions := knnReg.Predict(newInstance, 3)
 }
